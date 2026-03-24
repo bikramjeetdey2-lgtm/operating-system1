b556d9b846b10fc5406f3c1d4b0b4b3db4d7706c
bikramjeetdey2-lgtm <bikramjeetdey2@gmail.com>
2026-03-24 15:09:07 +0530

    Create Lab1a.C

---
+++ lab/Lab1a.C
@@ -0,0 +1,31 @@
+#include <stdio.h>   
+#include <unistd.h>   
+#include <fcntl.h>   
+   
+int main()   
+{     int fd;      
+char buffer[50];   
+   
+    /* open() – create and open a file */     fd = 
+open("sample.txt", O_CREAT | O_RDWR, 0644);   
+   
+    if (fd < 0)   
+    {   
+        prin ("File opening failed\n");         
+return 1;   
+    }   
+   
+    /* write() – write data into file */     
+write(fd, "Hello OS Lab", 12);   
+   
+    /* move file pointer to beginning */     
+lseek(fd, 0, SEEK_SET);   
+   
+/* read() – read data from file */     
+read(fd, buffer, 12);   
+/* display data */     prin ("Data read from 
+file: %s\n", buffer);   
+/* close() – close the file */     
+close(fd);   
+return 0;   
+}   
